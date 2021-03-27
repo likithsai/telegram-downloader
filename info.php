60b122e55782e26d1357141f808400e1427d83cf
likith sai <likithsai13@gmail.com>
2021-03-27 23:25:59 +0530

    Added remove from scheduler btn

--- info.php
+++ info.php
@@ -106,9 +106,16 @@
             </div>
          </nav>
          <section class="jumbotron bg-dark text-white mb-0 p-0 rounded-0">
-            <div class="container py-5">
-                <h3><?php echo (new telegramBot($bot_id))->getMe()["result"]["first_name"]; ?></h3>
-                <p class="lead"><?php echo '@' . (new telegramBot($bot_id))->getMe()["result"]["username"]; ?></p>
+            <div class="container py-5 justify-content-start d-md-flex d-sm-block text-center text-md-left">
+                <div class="d-block mr-md-4 mr-sm-0 mb-4 mb-md-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-collection" viewBox="0 0 16 16">
+                        <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13z"></path>
+                    </svg>
+                </div>
+                <div class="d-block">
+                    <h3>DealsAndSpecialOffers</h3>
+                    <p class="lead">@dealsandofferbot</p>
+                </div>
             </div>
         </section>
     </header>
@@ -156,7 +163,7 @@
                                                         </span>
                                                         <span>' . $username . '</span>
                                                     </h5>
-                                                    <p class="card-text text-muted text-small text-truncate">' . $content . '</p>
+                                                    <p class="card-text text-muted text-truncate">' . $content . '</p>
                                                 </div>
                                                 <div class="d-block mt-3 mt-md-0">                                                
                                                     <p class="d-flex justify-content-between align-middle">
@@ -177,24 +184,37 @@
                                         <div class="d-block">
                                             <div class="card-body d-flex justify-content-between align-middle">
                                                 <form method="post" action="<?php echo htmlspecialchars(getURL()); ?>">
-                                                    <button type="submit" name="enable_scheduler" class="btn btn-primary mt-1 col-sm-12 col-md-auto">
+                                                    <button type="submit" name="enable_scheduler" class="btn btn-primary mt-1 col-sm-12 col-md-auto mr-0 mr-md-2">
                                                         <span>
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                                                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                                             </svg>
                                                         </span>
                                                         <span class="ml-1">Delete Message</span>
-                                                    </button>
-                                                    <button type="submit" name="enable_scheduler" class="btn btn-primary mt-1 col-sm-12 col-md-auto">
-                                                        <span>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
-                                                                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
-                                                                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
-                                                            </svg>
-                                                        </span>
-                                                        <span class="ml-1">Add to Scheduler</span>
-                                                    </button>
-                                                </form>
+                                                    </button>';
+
+                                                    if($msg['msg_schedule']) {
+                                                        echo '<button type="submit" name="enable_scheduler" class="btn btn-primary mt-1 col-sm-12 col-md-auto">
+                                                            <span>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                                                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                                                </svg>
+                                                            </span>
+                                                            <span class="ml-1">Remove From Scheduler</span>
+                                                        </button>';
+                                                    } else {
+                                                        echo '<button type="submit" name="enable_scheduler" class="btn btn-primary mt-1 col-sm-12 col-md-auto">
+                                                            <span>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+                                                                    <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                                                                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+                                                                </svg>
+                                                            </span>
+                                                            <span class="ml-1">Add to Scheduler</span>
+                                                        </button>';
+                                                    }
+                                                echo '</form>
                                             </div>
                                         </div>
                                     </div>
