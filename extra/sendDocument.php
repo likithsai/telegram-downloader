bb9ca05e2f0041e4ea14cfdac8a4862ef6378e30
likithsai <likithsai13@gmail.com>
2021-04-17 21:43:25 +0530

    Add files via upload
    file upload extra

---
+++ extra/sendDocument.php
@@ -0,0 +1,11 @@
+<html>
+<body>	
+	<form method="POST" action="https://api.telegram.org/bot1642261514:AAGIyCFfX8UGkKLYEqGXKIjWOGLYLuXdd4U/sendDocument?chat_id=-1001436094290" enctype="multipart/form-data">
+	  <input type="text" name="caption" value="hi world"/>
+	  <br />
+	  <input type="file" name="document"/>
+	  <br />
+	  <input type="submit" name="submit" value="sendDocument" />
+	</form>
+</body>
+</html>
